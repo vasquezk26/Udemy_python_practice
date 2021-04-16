efd12e142f21f0a62fa2a9df606d95f2d0b5f90c
vasquezk26 <54609728+vasquezk26@users.noreply.github.com>
2021-04-16 19:47:53 -0400

    few lessons

---
+++ using_variable.py
@@ -0,0 +1,20 @@
+a = 10
+b = 5
+
+c = a+b
+d = b/a 
+
+print(c)
+print(d)
+
+import math
+math.sqrt(a)
+
+print(math.sqrt(a))
+
+greeting = "Hello"
+name = 'Bob'
+
+message = greeting + " " + name
+
+print(message)
